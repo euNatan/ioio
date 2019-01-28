581b47b2870b0b34ddca828f903fa7e28aa3c6f0
Your Name <you@example.com>
2019-01-28 08:46:51 -0200

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def top_collections
-    res = User.joins(:items).group("users.id").order("count(users.id) DESC")
+    res = User.joins(:items).where("items.status_id = ?", 1).group("users.id").order("count(users.id) DESC")
   end
 
 end
